3ec9a6b037e1fce973af814482d2b535b1cdb0a9
ChaosTherum <chaostherum@gmail.com>
2014-11-13 04:19:30 -0500

    Added a recipe for alien artifacts

---
+++ Alien-Artifact-Crafting/prototypes/recipe/item.lua
@@ -0,0 +1,14 @@
+data:extend({
+        {
+            type = "recipe",
+            name = "alien-artifact",
+            enabled = "true",
+            ingredients =
+            {
+                {"advanced-circuit", 20},
+                {"electronic-circuit", 50},
+                {"processing-unit", 10}
+            },
+            result = "alien-artifact"
+        }
+    })
